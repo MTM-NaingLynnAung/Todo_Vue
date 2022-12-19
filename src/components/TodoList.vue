38cf5855b9693598faeaf901c181dd8a3caeba23
NaingLynnAung <scm.nainglynnaung@gmail.com>
2022-12-19 17:05:31 +0630

    checkbox error

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,8 +5,9 @@
       <input type="text" class="form-control" v-model="newItem" @keyup.enter="addList" autofocus>
     </div>
     <ul class="mt-3 pt-3">
-      <li class="d-flex align-items-center position-relative mb-4" v-for="(todo, index) in todos" :key="todo.id" :id="todo.complete ? 'done' : 'active'">
-        <input type="checkbox" class="me-5 larger" v-model="todo.complete"> 
+      <li class="d-flex align-items-center position-relative mb-4" v-for="(todo, index) in filterTodo" :key="todo.id">
+        
+        <input type="checkbox" class="me-5 larger" v-model="todo.complete">
         <div @dblclick="edit(index)" @keyup.enter="removeInput(index)" class="w-100 d-flex align-items-center">
           <label :class="todo.complete ? 'line' : '' " class="w-100" style="text-align: left;">{{ todo.title }}</label>
           <span class="position-absolute size" @click="deleteItem(index)">&times;</span>
@@ -18,21 +19,19 @@
     <hr v-show="todos.length">
       <div class="d-flex justify-content-between align-items-center">
         <span class=""> {{ activeItem.length }} item left</span>
-        <ul class="d-flex justify-content-between m-0 col-5" style="font-size:1rem">
-          <li>
-            <a href="#" @click="all">All</a>
+        <ul class="d-flex justify-content-between m-0 col-5 p-0" style="font-size:1rem">
+          <li class="link">
+            <router-link to="/all">All</router-link>
           </li>
-          <li>
-            <a href="#active" @click="active">Active</a>
+          <li class="link">
+            <router-link to="/active">Active</router-link>
           </li>
-          <li>
-            <a href="#completed" @click="completed">Completed</a>
+          <li class="link">
+            <router-link to="/completed">Completed</router-link>
           </li>
         </ul>
         <span class="col-3"><a href="javascript:void(0)" @click="clear" v-show="completeItem.length > 0">Clear Completed</a></span>
       </div>
-
-    
   </div>
 </template>
 <script>
@@ -52,7 +51,7 @@ export default {
   },
   methods: {
     addList(){
-      let todo = { title: this.newItem, complete: false, type: 'hidden' }
+      let todo = {id: this.todos.length + 1, title: this.newItem, complete: false, type: 'hidden' }
       this.todos = [...this.todos,  todo]
       this.newItem = ''
     },
@@ -60,34 +59,10 @@ export default {
       this.todos.splice(index, 1)
     },
     edit(index){
-      this.todos[index].type = 'text'
+      this.filterTodo[index].type = 'text'
     },
     removeInput(index){
-      this.todos[index].type = 'hidden'
-    },
-    all(){
-      document.querySelectorAll("#active").forEach(e => {
-        e.classList.remove('none')
-      })
-      document.querySelectorAll("#done").forEach(e => {
-        e.classList.remove('none')
-      })
-    },
-    active(){
-      document.querySelectorAll("#done").forEach(e => {
-        e.classList.add('none')
-      })
-      document.querySelectorAll("#active").forEach(e => {
-        e.classList.remove('none')
-      })
-    },
-    completed(){
-      document.querySelectorAll("#active").forEach(e => {
-        e.classList.add('none')
-      })
-      document.querySelectorAll("#done").forEach(e => {
-        e.classList.remove('none')
-      })
+      this.filterTodo[index].type = 'hidden'
     },
     clear() {
       this.todos = this.activeItem
@@ -103,20 +78,32 @@ export default {
       return this.todos.filter(element => {
         return element.complete == true
       })
+    },
+    filterTodo(){
+      return this.todos.filter((element) => {
+        if (this.$route.path == '/active'){
+          return element.complete == false
+        }else if (this.$route.path == '/completed'){
+           return element.complete == true
+        }else {
+          return element
+        }
+      })
     }
   },
-  mounted() {  
-    if (localStorage.getItem('todos')) {
+  mounted(){
+    if(localStorage.getItem('todos')){
       this.todos = JSON.parse(localStorage.getItem('todos'))
     }
   },
+  
   watch: {
     todos: {
       handler() {
         localStorage.setItem('todos', JSON.stringify(this.todos))
       },
-      deep: true
-    }
+      deep: true,
+    },
   }
 }
 </script>
@@ -146,7 +133,10 @@ a{
     position: absolute;
     padding-left: 12px;
  }
- ul .none {
-  display: none!important;
+ .link a{
+  padding: 5px 10px;
+ }
+ .link a:hover, .router-link-active {
+    border-bottom: 2px solid #1ad59d;
  }
 </style>
